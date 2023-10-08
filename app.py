8b675f6c8e992547c6144c3c29ea335c2270fd95
Kushaj Arora <84903226+Kushajarora@users.noreply.github.com>
2023-10-08 14:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
@@ -65,7 +65,7 @@ def handle_userinput(user_question):
 
 
 def main():
-#    load_dotenv()
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
